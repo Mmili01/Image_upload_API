48ff65199c3271707f40936d32c318556be14f20
Abba Emmanuel <eabba21@gmail.com>
2023-09-08 20:54:55 +0100

    added the upload path

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,21 +3,13 @@ const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 
 const createProduct = async (req, res) => {
-  res.send("create product");
+  console.log(req.body);
+
+  const product = await Product.create(req.body);
+  res.status(StatusCodes).json({ product });
 };
 const getAllProduct = async (req, res) => {
   res.send("get all product");
 };
-// const getSingleProduct = async (req, res)=>{
-//     res.send('get single product')
-// }
-// const updateProduct = async (req, res)=>{
-//     res.send('update product')
-// }
 
-// const deleteProduct = async (req, res)=>{
-//     res.send('delete product')
-// }
-
-
- module.exports = {createProduct, getAllProduct}
+module.exports = { createProduct, getAllProduct };
